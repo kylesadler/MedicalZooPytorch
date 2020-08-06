eacebf558b090c3085778a8b98fbe3b1b93bd436
kyle sadler <kylesadler5@gmail.com>
2020-08-06 15:59:50 -0500

    update

--- run.sh
+++ run.sh
@@ -1,16 +1,8 @@
 cd /home/kyle/MedicalZooPytorch
 git pull
 
-for i in {1..4}
+for i in {1..10}
 do
     echo $i
-    nohup python train_unet_iseg19.py --fold_id $i > unet_iseg19_$i.nohup 2>&1 &
-done
-
-nohup python train_unet_iseg19.py --fold_id 5 > unet_iseg19_5.nohup 2>&1
-
-for i in {6..10}
-do
-    echo $i
-    nohup python train_unet_iseg19.py --fold_id $i > unet_iseg19_$i.nohup 2>&1 &
+    nohup python train_unet_iseg19.py --fold_id $i > unet_iseg19_$i.nohup 2>&1
 done
