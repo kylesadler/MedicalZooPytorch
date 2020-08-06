27e4474d01a7a4b4740e179ba52dec2093b15675
kyle sadler <kylesadler5@gmail.com>
2020-08-06 15:52:39 -0500

    update

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ git pull
 for i in {1..10}
 do
     echo $i
-    nohup python train_unet_iseg19.py --fold_id $i > unet_iseg19_$i.nohup 2>&1 &;
+    nohup python train_unet_iseg19.py --fold_id $i > unet_iseg19_$i.nohup 2>&1 &
 done
