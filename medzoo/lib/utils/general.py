ce63c59d5d7833198ca02b5bf407a5a7141988ed
kyle sadler <kylesadler5@gmail.com>
2020-08-09 12:25:19 -0500

    bug fix

--- medzoo/lib/utils/general.py
+++ medzoo/lib/utils/general.py
@@ -67,9 +67,16 @@ def prepare_input(input_tuple, inModalities=-1, inChannels=-1, cuda=False, args=
         if channels == 3:
             img_1, img_2, img_3, target = input_tuple
             print(len(img_1))
+            print(len(img_1[0]))
+            
             print(len(img_2))
+            print(len(img_2[0]))
+
             print(len(img_3))
+            print(len(img_3[0]))
+
             print(len(target))
+            print(len(target[0]))
             input_tensor = torch.cat((img_1, img_2, img_3), dim=1)
         elif channels == 2:
             img_1, img_2, _, target = input_tuple
