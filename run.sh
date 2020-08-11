98afabdcb2f867e1778dc3b4132b33346830b9de
kyle sadler <kylesadler5@gmail.com>
2020-08-10 21:57:10 -0500

    finished brats2019

--- run.sh
+++ run.sh
@@ -6,3 +6,6 @@ do
     echo $i
     nohup python train_unet_iseg19.py --fold_id $i > unet_iseg19_$i.nohup 2>&1
 done
+
+
+# rm ~/results/UNET3D/brats2019_1_11-08_02-51/ -r; rm unet_brats2019.nohup;git pull; nohup python train_unet_brats2019.py > unet_brats2019.nohup 2>&1 &
