7bced45dc7bf0e3156b90daa9e84829487032170
kyle sadler <kylesadler5@gmail.com>
2020-09-06 17:41:33 -0500

    formatted

--- util/args.py
+++ util/args.py
@@ -1,58 +1,104 @@
 import time
 import argparse
 
+
 def datestr():
     now = time.gmtime()
-    return '{:02}-{:02}_{:02}-{:02}'.format(now.tm_mday, now.tm_mon, now.tm_hour, now.tm_min)
+    return "{:02}-{:02}_{:02}-{:02}".format(
+        now.tm_mday, now.tm_mon, now.tm_hour, now.tm_min
+    )
 
 
 def get_default_parser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('train', type=str, default='train')
-    parser.add_argument('test', type=str, default='test')
-    parser.add_argument('--nEpochs', type=int, default=20000)
-    parser.add_argument('--batchSz', type=int, default=8)
+    parser.add_argument("train", type=str, default="train")
+    parser.add_argument("test", type=str, default="test")
+    parser.add_argument("--nEpochs", type=int, default=20000)
+    parser.add_argument("--batchSz", type=int, default=8)
 
-    parser.add_argument('--samples_train', type=int, default=1024)
-    parser.add_argument('--samples_val', type=int, default=128)
+    parser.add_argument("--samples_train", type=int, default=1024)
+    parser.add_argument("--samples_val", type=int, default=128)
     # parser.add_argument('--samples_train', type=int, default=1)
     # parser.add_argument('--samples_val', type=int, default=1)
-    
 
-    parser.add_argument('--model', type=str, default='UNET3D', choices=('VNET', 'VNET2', 'UNET3D', 'DENSENET1', 'DENSENET2', 'DENSENET3', 'HYPERDENSENET'))
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="UNET3D",
+        choices=(
+            "VNET",
+            "VNET2",
+            "UNET3D",
+            "DENSENET1",
+            "DENSENET2",
+            "DENSENET3",
+            "HYPERDENSENET",
+        ),
+    )
 
-    parser.add_argument('--opt', type=str, default='sgd', choices=('sgd', 'adam', 'rmsprop'))
-    parser.add_argument('--log_dir', type=str, default='./runs/')
-    parser.add_argument('--cuda', action='store_true', default=True)
+    parser.add_argument(
+        "--opt", type=str, default="sgd", choices=("sgd", "adam", "rmsprop")
+    )
+    parser.add_argument("--log_dir", type=str, default="./runs/")
+    parser.add_argument("--cuda", action="store_true", default=True)
 
-    parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+    parser.add_argument(
+        "--resume",
+        default="",
+        type=str,
+        metavar="PATH",
+        help="path to latest checkpoint (default: none)",
+    )
 
-    parser.add_argument('--augmentation', default=True, type=str, help='Tensor normalization: options max, mean, global')
-    parser.add_argument('--normalization', default='full_volume_mean', type=str, help='Tensor normalization: options max, mean, global')
+    parser.add_argument(
+        "--augmentation",
+        default=True,
+        type=str,
+        help="Tensor normalization: options max, mean, global",
+    )
+    parser.add_argument(
+        "--normalization",
+        default="full_volume_mean",
+        type=str,
+        help="Tensor normalization: options max, mean, global",
+    )
 
     return parser
+
 
 def get_parser(**kwargs):
     """ fold_id, loadData, split """
     parser = get_default_parser()
 
-    parser.add_argument('--loadData', default=kwargs['loadData'])
-    parser.add_argument('--split', default=kwargs['split'], type=float, help='Select percentage of training data(default: 0.8)')
-    parser.add_argument('--fold_id', default=str(kwargs['fold_id']), type=str, help='Select subject for fold validation')
+    parser.add_argument("--loadData", default=kwargs["loadData"])
+    parser.add_argument(
+        "--split",
+        default=kwargs["split"],
+        type=float,
+        help="Select percentage of training data(default: 0.8)",
+    )
+    parser.add_argument(
+        "--fold_id",
+        default=str(kwargs["fold_id"]),
+        type=str,
+        help="Select subject for fold validation",
+    )
 
+    parser.add_argument("--dataset_name", type=str, default=kwargs["dataset_name"])
+    parser.add_argument("--dim", nargs="+", type=int, default=kwargs["dim"])
+    parser.add_argument("--classes", type=int, default=kwargs["classes"])
 
-    parser.add_argument('--dataset_name', type=str, default=kwargs['dataset_name'])
-    parser.add_argument('--dim', nargs="+", type=int, default=kwargs['dim'])
-    parser.add_argument('--classes', type=int, default=kwargs['classes'])
+    parser.add_argument("--inChannels", type=int, default=kwargs["inChannels"])
+    parser.add_argument("--inModalities", type=int, default=kwargs["inModalities"])
+    parser.add_argument("--threshold", default=kwargs["threshold"], type=float)
 
-    parser.add_argument('--inChannels', type=int, default=kwargs['inChannels'])
-    parser.add_argument('--inModalities', type=int, default=kwargs['inModalities'])
-    parser.add_argument('--threshold', default=kwargs['threshold'], type=float)
-
-    parser.add_argument('--lr', default=kwargs['lr'], type=float, help='learning rate (default: 1e-3)')
+    parser.add_argument(
+        "--lr", default=kwargs["lr"], type=float, help="learning rate (default: 1e-3)"
+    )
 
     return parser
+
 
 """ 
 def mrbrains9_arguments(loadData=False):
@@ -110,15 +156,15 @@ def brats2019_arguments(fold_id, loadData):
 
 
 def to_args(kwargs):
-    """ generates argparse arguments from a keyword configuration dict
+    """generates argparse arguments from a keyword configuration dict
 
     Args:
         kwargs (dict): dictionary of keyword arguments to pass to get_parser()
 
     Returns:
         argparse arguments: configuration arguments
-    """        
+    """
     parser = get_parser(**kwargs)
     args = parser.parse_args()
-    args.save = f'/home/kyle/results/{args.model}/{args.dataset_name}_{args.fold_id}_{datestr()}'
+    args.save = f"/home/kyle/MedicalZooPytorch/results/{args.model}/{args.dataset_name}_{args.fold_id}_{datestr()}"
     return args
