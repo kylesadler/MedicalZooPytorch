9349e4db66ecb9db7655422f2d2508c4e864b818
kyle sadler <kylesadler5@gmail.com>
2020-09-06 18:19:53 -0500

    formatted

--- medzoo/lib/medloaders/iseg2019.py
+++ medzoo/lib/medloaders/iseg2019.py
@@ -8,18 +8,31 @@ from torch.utils.data import Dataset
 import medzoo.lib.augment3D as augment3D
 import medzoo.lib.utils as utils
 from medzoo.lib.medloaders import medical_image_process as img_loader
-from medzoo.lib.medloaders.medical_loader_utils import get_viz_set, create_sub_volumes, create_non_overlapping_sub_volumes
+from medzoo.lib.medloaders.medical_loader_utils import (
+    get_viz_set,
+    create_sub_volumes,
+    create_non_overlapping_sub_volumes,
+)
+
 
 class MRIDatasetISEG2019(Dataset):
     """
     Code for reading the infant brain MRI dataset of ISEG 2017 challenge
     """
 
-    def __init__(self, args, mode, dataset_path='./datasets', crop_dim=(32, 32, 32), split_id=1, samples=1000,
-                 load=False):
+    def __init__(
+        self,
+        args,
+        mode,
+        dataset_path="./datasets",
+        crop_dim=(32, 32, 32),
+        split_id=1,
+        samples=1000,
+        load=False,
+    ):
         # split_id = int(split_id)
         fold_id = int(args.fold_id)
-        print(f'using fold_id {fold_id}')
+        print(f"using fold_id {fold_id}")
         """
         :param mode: 'train','val','test'
         :param dataset_path: root dataset folder
@@ -29,8 +42,8 @@ class MRIDatasetISEG2019(Dataset):
         """
         self.mode = mode
         self.root = str(dataset_path)
-        self.training_path = self.root + '/iseg_2019/iSeg-2019-Training/'
-        self.testing_path = self.root + '/iseg_2019/iSeg-2019-Validation/'
+        self.training_path = self.root + "/iseg_2019/iSeg-2019-Training/"
+        self.testing_path = self.root + "/iseg_2019/iSeg-2019-Validation/"
         self.CLASSES = 4
         self.full_vol_dim = (144, 192, 256)  # slice, width, height
         self.crop_size = crop_dim
@@ -40,83 +53,127 @@ class MRIDatasetISEG2019(Dataset):
         self.list = []
         self.samples = int(samples)
         self.full_volume = None
-        self.save_name = self.root + '/iseg_2019/iseg2019-list-' + mode + '-samples-' + str(samples) + '.txt'
+        self.save_name = (
+            self.root
+            + "/iseg_2019/iseg2019-list-"
+            + mode
+            + "-samples-"
+            + str(samples)
+            + ".txt"
+        )
         if self.augmentation:
             self.transform = augment3D.RandomChoice(
-                transforms=[augment3D.GaussianNoise(mean=0, std=0.01), augment3D.RandomFlip(),
-                            augment3D.ElasticTransform()], p=0.5)
+                transforms=[
+                    augment3D.GaussianNoise(mean=0, std=0.01),
+                    augment3D.RandomFlip(),
+                    augment3D.ElasticTransform(),
+                ],
+                p=0.5,
+            )
         if load:
             ## load pre-generated data
             self.list = utils.load_list(self.save_name)
-            list_IDsT1 = sorted(glob.glob(os.path.join(self.training_path, '*T1.img')))
+            list_IDsT1 = sorted(glob.glob(os.path.join(self.training_path, "*T1.img")))
             self.affine = img_loader.load_affine_matrix(list_IDsT1[0])
             return
 
-        subvol = '_vol_' + str(crop_dim[0]) + 'x' + str(crop_dim[1]) + 'x' + str(crop_dim[2])
-        self.sub_vol_path = self.root + '/iseg_2019/generated/' + mode + subvol + '/'
+        subvol = (
+            "_vol_" + str(crop_dim[0]) + "x" + str(crop_dim[1]) + "x" + str(crop_dim[2])
+        )
+        self.sub_vol_path = self.root + "/iseg_2019/generated/" + mode + subvol + "/"
         utils.make_dirs(self.sub_vol_path)
 
-        list_IDsT1 = sorted(glob.glob(os.path.join(self.training_path, '*T1.img')))
-        list_IDsT2 = sorted(glob.glob(os.path.join(self.training_path, '*T2.img')))
-        labels = sorted(glob.glob(os.path.join(self.training_path, '*label.img')))
+        list_IDsT1 = sorted(glob.glob(os.path.join(self.training_path, "*T1.img")))
+        list_IDsT2 = sorted(glob.glob(os.path.join(self.training_path, "*T2.img")))
+        labels = sorted(glob.glob(os.path.join(self.training_path, "*label.img")))
         print(self.training_path)
         self.affine = img_loader.load_affine_matrix(list_IDsT1[0])
 
-        if self.mode == 'train':
+        if self.mode == "train":
             # custom code
             # list_IDsT1 = list_IDsT1[:split_id]
             # list_IDsT2 = list_IDsT2[:split_id]
             # labels = labels[:split_id]
 
-            list_IDsT1 = [ x for x in list_IDsT1 if f'-{fold_id}-' not in x ]
-            list_IDsT2 = [ x for x in list_IDsT2 if f'-{fold_id}-' not in x ]
-            labels = [ x for x in labels if f'-{fold_id}-' not in x ]
+            list_IDsT1 = [x for x in list_IDsT1 if f"-{fold_id}-" not in x]
+            list_IDsT2 = [x for x in list_IDsT2 if f"-{fold_id}-" not in x]
+            labels = [x for x in labels if f"-{fold_id}-" not in x]
 
-            assert(len(labels) == len(list_IDsT1))
-            assert(len(labels) == len(list_IDsT2))
-            assert(len(labels) == 9)
+            assert len(labels) == len(list_IDsT1)
+            assert len(labels) == len(list_IDsT2)
+            assert len(labels) == 9
 
-            self.list = create_sub_volumes(list_IDsT1, list_IDsT2, labels, dataset_name="iseg2019",
-                                           mode=mode, samples=samples, full_vol_dim=self.full_vol_dim,
-                                           crop_size=self.crop_size,
-                                           sub_vol_path=self.sub_vol_path, th_percent=self.threshold)
+            self.list = create_sub_volumes(
+                list_IDsT1,
+                list_IDsT2,
+                labels,
+                dataset_name="iseg2019",
+                mode=mode,
+                samples=samples,
+                full_vol_dim=self.full_vol_dim,
+                crop_size=self.crop_size,
+                sub_vol_path=self.sub_vol_path,
+                th_percent=self.threshold,
+            )
 
-        elif self.mode == 'val':
+        elif self.mode == "val":
             # list_IDsT1 = list_IDsT1[split_id:]
             # list_IDsT2 = list_IDsT2[split_id:]
             # labels = labels[split_id:]
 
-            list_IDsT1 = [ x for x in list_IDsT1 if f'-{fold_id}-' in x ]
-            list_IDsT2 = [ x for x in list_IDsT2 if f'-{fold_id}-' in x ]
-            labels = [ x for x in labels if f'-{fold_id}-' in x ]
-            assert(len(labels) == len(list_IDsT1))
-            assert(len(labels) == len(list_IDsT2))
-            assert(len(labels) == 1)
+            list_IDsT1 = [x for x in list_IDsT1 if f"-{fold_id}-" in x]
+            list_IDsT2 = [x for x in list_IDsT2 if f"-{fold_id}-" in x]
+            labels = [x for x in labels if f"-{fold_id}-" in x]
+            assert len(labels) == len(list_IDsT1)
+            assert len(labels) == len(list_IDsT2)
+            assert len(labels) == 1
 
-            self.list = create_sub_volumes(list_IDsT1, list_IDsT2, labels, dataset_name="iseg2019",
-                                           mode=mode, samples=samples, full_vol_dim=self.full_vol_dim,
-                                           crop_size=self.crop_size,
-                                           sub_vol_path=self.sub_vol_path, th_percent=self.threshold)
+            self.list = create_sub_volumes(
+                list_IDsT1,
+                list_IDsT2,
+                labels,
+                dataset_name="iseg2019",
+                mode=mode,
+                samples=samples,
+                full_vol_dim=self.full_vol_dim,
+                crop_size=self.crop_size,
+                sub_vol_path=self.sub_vol_path,
+                th_percent=self.threshold,
+            )
 
-            self.full_volume = get_viz_set(list_IDsT1, list_IDsT2, labels, dataset_name="iseg2019")
+            self.full_volume = get_viz_set(
+                list_IDsT1, list_IDsT2, labels, dataset_name="iseg2019"
+            )
 
-        elif self.mode == 'test':
+        elif self.mode == "test":
             # self.list_IDsT1 = sorted(glob.glob(os.path.join(self.testing_path, '*T1.img')))
             # self.list_IDsT2 = sorted(glob.glob(os.path.join(self.testing_path, '*T2.img')))
             # self.labels = None
             # todo inference here
 
-            list_IDsT1 = [ x for x in list_IDsT1 if f'-{fold_id}-' in x ]
-            list_IDsT2 = [ x for x in list_IDsT2 if f'-{fold_id}-' in x ]
-            labels = [ x for x in labels if f'-{fold_id}-' in x ]
-            assert(len(labels) == len(list_IDsT1))
-            assert(len(labels) == len(list_IDsT2))
-            assert(len(labels) == 1)
-            
-            self.list = create_non_overlapping_sub_volumes(list_IDsT1, list_IDsT2, labels, dataset_name="iseg2019", mode=mode, samples=samples, full_vol_dim=self.full_vol_dim, crop_size=self.crop_size, sub_vol_path=self.sub_vol_path, th_percent=self.threshold)
+            list_IDsT1 = [x for x in list_IDsT1 if f"-{fold_id}-" in x]
+            list_IDsT2 = [x for x in list_IDsT2 if f"-{fold_id}-" in x]
+            labels = [x for x in labels if f"-{fold_id}-" in x]
+            assert len(labels) == len(list_IDsT1)
+            assert len(labels) == len(list_IDsT2)
+            assert len(labels) == 1
 
-            self.full_volume = get_viz_set(list_IDsT1, list_IDsT2, labels, dataset_name="iseg2019")
+            self.list = create_non_overlapping_sub_volumes(
+                list_IDsT1,
+                list_IDsT2,
+                labels,
+                dataset_name="iseg2019",
+                mode=mode,
+                samples=samples,
+                full_vol_dim=self.full_vol_dim,
+                crop_size=self.crop_size,
+                sub_vol_path=self.sub_vol_path,
+                th_percent=self.threshold,
+            )
 
+            self.full_volume = get_viz_set(
+                list_IDsT1, list_IDsT2, labels, dataset_name="iseg2019"
+            )
 
         utils.save_list(self.save_name, self.list)
 
@@ -126,10 +183,17 @@ class MRIDatasetISEG2019(Dataset):
     def __getitem__(self, index):
         t1_path, t2_path, seg_path = self.list[index]
         t1, t2, s = np.load(t1_path), np.load(t2_path), np.load(seg_path)
-        if self.mode == 'train' and self.augmentation:
+        if self.mode == "train" and self.augmentation:
             [augmented_t1, augmented_t2], augmented_s = self.transform([t1, t2], s)
 
-            return torch.FloatTensor(augmented_t1.copy()).unsqueeze(0), torch.FloatTensor(
-                augmented_t2.copy()).unsqueeze(0), torch.FloatTensor(augmented_s.copy())
+            return (
+                torch.FloatTensor(augmented_t1.copy()).unsqueeze(0),
+                torch.FloatTensor(augmented_t2.copy()).unsqueeze(0),
+                torch.FloatTensor(augmented_s.copy()),
+            )
 
-        return torch.FloatTensor(t1).unsqueeze(0), torch.FloatTensor(t2).unsqueeze(0), torch.FloatTensor(s)
+        return (
+            torch.FloatTensor(t1).unsqueeze(0),
+            torch.FloatTensor(t2).unsqueeze(0),
+            torch.FloatTensor(s),
+        )
